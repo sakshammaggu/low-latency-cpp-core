8289e962080f6a54b14e83b2919fcc039facd0c1
Saksham-Maggu <sakshammaggu80@gmail.com>
2026-05-31 21:21:55 +0000

    bool datatype

---
+++ 01-DataTypes/01-bool/bool.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    bool isTrue = true;
+    bool isFalse = false;
+    cout << "isTrue: " << isTrue << endl;
+    cout << "isFalse: " << isFalse << endl;
+}
