baee26efedf3bda1a4f909adbeadc03bf8f07485
Saksham-Maggu <sakshammaggu80@gmail.com>
2026-06-04 19:50:31 +0000

    string datatype

---
+++ 01-DataTypes/04-string/string.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    string isTrue = "True";
+    string isFalse = "False";
+    cout << "isTrue: " << isTrue << endl;
+    cout << "isFalse: " << isFalse << endl;
+}
