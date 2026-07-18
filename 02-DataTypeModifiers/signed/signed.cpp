1fdc814108032068709b27eea7ccb049529a0504
Saksham-Maggu <sakshammaggu80@gmail.com>
2026-07-18 16:44:51 +0000

    signed type modifier

---
+++ 02-DataTypeModifiers/signed/signed.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    signed int a = -10; // signed integer
+    cout << "Value of a: " << a << endl;
+    cout << "Size of a: " << sizeof(a) << " bytes" << endl;
+    cout << "Size of signed int: " << sizeof(signed int) << " bytes" << endl;
+    return 0;
+}
